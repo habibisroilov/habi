4b85d8f1de9de87ad94edf88916163c2f072278e
Habi Isroilov <habiisroilov345@gmail.com>
2021-11-13 19:14:54 +0500

    Adde some code

--- index.py
+++ index.py
@@ -1,5 +1,29 @@
-from random import random
-from math import floor 
-print(floor(random()*100))
+# from random import random
+# from math import floor 
+# print(floor(random()*100))
 # for letter in 'Habi':
 #     print(letter)
+
+# names = ['Xasan','Xusan','Elbek']
+# print(len(names)) ***************** uzunligini*******************
+
+# print()
+# names = ['xasan','xusan','elbek','naima']
+# print(names[2::-1])
+# from random import randint
+# num = int(input("Enter numbeir:"))
+# list = []
+# i =  0
+# while i < num :
+#     list.append(randint(0,100))
+#     i += 1
+    
+# print(list)
+
+# a = [[1,2,3],[4,5,6,],[7,8,9]]
+# for line in a:
+#     for elem in line :
+#         print(elem)
+
+
+
