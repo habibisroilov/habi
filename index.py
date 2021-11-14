b695c71c1e66c34c0d9215b014e5f47b52a0568f
Habi Isroilov <habiisroilov345@gmail.com>
2021-11-14 16:55:39 +0500

    Added some code

--- index.py
+++ index.py
@@ -17,13 +17,106 @@
 # while i < num :
 #     list.append(randint(0,100))
 #     i += 1
-    
-# print(list)
 
-# a = [[1,2,3],[4,5,6,],[7,8,9]]
-# for line in a:
-#     for elem in line :
-#         print(elem)
+
+
+
+# # print(list)
+
+# # a = [[1,2,3],[4,5,6,],[7,8,9]]
+# # for line in a:
+# #     for elem in line :
+# #         print(elem)
+
+
+
+# # print("Hello world")
+# # print('Assalomu alaykum ')
+# # print('Xayrli tong')
+# # print(2+4*2)
+# # print(19/5)
+# # print(2**4)
+# # print("""YAxshi otga bir qamchi,
+
+# # Yomon otga ming qamchi""")
+# # print("nexia","tico","damas","korganlar qilar havas.")
+# # print(5**4)
+# # print(22%4)
+# # print(2*12*3.14159)
+# # radius = 5
+# # pi = 3.14159
+# # yuza = pi *radius**2
+# # print('radiusi',radius,"ga teng doira yuzi",yuza)
+# # matn = "hello world"
+# # print(matn)
+# # ism = 'abdullo'
+# # surname = 'isroilov'
+# # ism_surname = f"{ism} {surname}"
+# # print(ism_surname)
+# yil = 2002
+# print(f'siz {yil} tugilgansiz ')
+# print(f"Yoshingiz {2021-yil} da")
+# ism = 'HabiBullO'
+# sharif = 'IsroIlov'
+# ismsharif=f"{ism} {sharif}"
+# print(ismsharif.upper())#Barcha harflarni kattada yozadi 
+# print(ismsharif.lower())#Barcha harflarni kichkinada yozad
+# print(ismsharif.title())#Faqat bosh hariflani kattada
+# print(ismsharif.capitalize())#faqat boshidagini kattada
+# meva = '      olama       '
+# print('man '+ meva.strip(),'yaxshi koraman')
+
+# ism = input('Ism nima ')
+# print(f"Salom  {ism.title()}")
+# kocha ='bogbon'
+# mahalla = 'sagbon'
+# tuman = 'bodomzor'
+# viloyat = 'samarqand'
+# print( kocha.upper(),'kochasi\n',mahalla.upper(),'mahhalasi\n',tuman.upper(),'Tumani\n',viloyat.upper()," viloyati")
+
+
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# num = lst.pop(3)
+# print('Menda ',num, "ta tel bor")
+ 
+# lst = ['alma','anor','bexi','nik','hurmo']
+# meva = lst.append('tarvuz')
+# print(lst)
+
+# lst = ['malibu','tiko','nexia','genttra']
+# del lst[0]
+# print(lst)
+
+# cars = int(input('soon kirit'))
+# cars.sort(reverse=True)
+# cars.sort()
+# print(cars)
+
+for i in range(1,100):
+    sum = i*i
+    print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
